cd0847df498e9874f5793a61d80c9fee3e3e4081
hsldymq <hsldymq@qq.com>
2019-03-06 23:53:49 +0800

    update worker factory

--- src/WorkerFactory.php
+++ src/WorkerFactory.php
@@ -7,8 +7,44 @@ use Archman\Whisper\Interfaces\WorkerFactoryInterface;
 
 class WorkerFactory implements WorkerFactoryInterface
 {
+    /**
+     * @var array [
+     *      [$signal, $handler],
+     *      ...
+     * ]
+     */
+    private $signalHandlers = [];
+
+    /**
+     * @var array [
+     *      [$event, $handler],
+     *      ...
+     * ]
+     */
+    private $eventHandlers = [];
+
     public function makeWorker(string $id, $socketFD): AbstractWorker
     {
-        return new Processor($id, $socketFD);
+        $p = new Processor($id, $socketFD);
+
+        foreach ($this->signalHandlers as $s => $h) {
+            $p->addSignalHandler($s, $h);
+        }
+
+        foreach ($this->eventHandlers as $e => $h) {
+            $p->on($e, $h);
+        }
+
+        return $p;
+    }
+
+    public function registerSignal(int $signal, callable $handler)
+    {
+        $this->signalHandlers[] = [$signal, $handler];
+    }
+
+    public function registerEvent(string $event, callable $handler)
+    {
+        $this->eventHandlers[] = [$event, $handler];
     }
 }
