17dc9898cf5a25637b5de39cdf672b2d38e3024d
hsldymq <hsldymq@qq.com>
2019-03-16 18:05:30 +0800

    update comment

--- src/EventEmitterTrait.php
+++ src/EventEmitterTrait.php
@@ -6,7 +6,7 @@ trait EventEmitterTrait
 {
     public function emit($event, array $arguments = [])
     {
-        // 非AbstractMaster预定义事件就加上当前对象在参数末尾
+        // 非Whisper预定义事件就加上当前对象在参数末尾
         if (strpos($event, '__') !== 0) {
             $arguments[] = $this;
         }
