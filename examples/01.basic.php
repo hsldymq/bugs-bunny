ca9b626d8d812864b7cd5da3f4a31fcfd1b6001b
hsldymq <hsldymq@qq.com>
2019-03-21 18:38:16 +0800

    更新示例说明

--- examples/01.basic.php
+++ examples/01.basic.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * 在这个示例中,展示了最基本的流程.
+ * 1. 向WorkerFactory注册worker的事件处理器,信号处理器以及最关键的amqp消息处理器
+ * 2. Dispatcher实际上就是一个master,它监听来自Connection的amqp消息,并将消息投放给当前空闲的worker进程,如果尚无空闲,就fork一个
+ *    fork的进程使用WorkerFactory来创建一个Worker实例来处理所有投放过来的amqp消息
+ *    Dispatcher也可以注册/监听事件,信号处理器
+ *
+ * 这个示例不限制fork的worker数量,所以你可以调整usleep的值,来看看在海量消息到来的时候,worker的消息处理能力对fork数量的影响.
+ * 建议不要把值设得太高,否则系统资源可能会被fork耗完
+ *
+ * 示例02中,你会看到如何限制worker的数量上限.
+ */
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Archman\BugsBunny\QueueMessage;
@@ -58,5 +71,5 @@ $dispatcher->addSignalHandler(SIGINT, function () use ($dispatcher) {
     $dispatcher->shutdown();
 });
 
-echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+echo "Waiting for messages. To exit press CTRL+C\n";
 $dispatcher->run();
