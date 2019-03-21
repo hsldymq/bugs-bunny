e37aedfeaa2fad982cf4272effaa7feef2a8e49b
hsldymq <hsldymq@qq.com>
2019-03-21 16:39:27 +0800

    用例中发布消息的脚本增加每秒定时发布消息的功能

--- examples/00.publish_msg.php
+++ examples/00.publish_msg.php
@@ -2,45 +2,98 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Bunny\Channel;
 use Bunny\Client;
+use Archman\Whisper\AbstractMaster;
 
-if (count($argv) < 3) {
-    echo "Usage: php 00.publish_msg.php 10 1000\n";
-    echo "\t10: number of message.\n";
-    echo "\t1000: content length(bytes).\n";
-    exit();
-}
+(new class () extends AbstractMaster {
+    private $numMessages;
 
-$exchange = 'bugsbunny';
-$queues = [
-    'queue1' => 'queue1',
-    'queue2' => 'queue2',
-    'queue3' => 'queue3',
-];
+    private $contentLength;
 
-$client = new Client([
-    'host' => '127.0.0.1',
-    'port' => 5672,
-    'vhost' => '/',
-    'user' => 'guest',
-    'password' => 'guest',
-]);
-$client->connect();
-$channel = $client->channel();
-$channel->exchangeDeclare($exchange);
-foreach ($queues as $routingKey => $queue) {
-    $channel->queueDeclare($queue);
-    $channel->queueBind($queue, $exchange, $routingKey);
-}
+    private $periodic;
 
+    private $connectionOptions = [
+        'host' => '127.0.0.1',
+        'port' => 5672,
+        'vhost' => '/',
+        'user' => 'guest',
+        'password' => 'guest',
+    ];
 
-$numMsg = intval($argv[1]);
-$routingKeys = array_keys($queues);
-for ($i = 0; $i < $numMsg; $i++) {
-    $key = $routingKeys[array_rand($routingKeys)];
+    /**
+     * @var Client
+     */
+    private $client;
 
-    $content = str_repeat('x', intval($argv[2]));
-    $channel->publish($content, [], $exchange, $key);
-}
+    /**
+     * @var Channel
+     */
+    private $channel;
 
-echo "done!\n";
+    private $exchange = "bugsbunny";
+
+    private $queues = [
+        'queue1' => 'queue1',
+        'queue2' => 'queue2',
+        'queue3' => 'queue3',
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        global $argv;
+
+        if (count($argv) < 3) {
+            echo "Usage: php 00.publish_msg.php 10 1000 0\n";
+            echo "\t10: number of message.\n";
+            echo "\t1000: content length(bytes).\n";
+            echo "\t0: periodic(per second), 1:true, 0:false. this is optional.";
+            exit();
+        }
+
+        $this->numMessages = intval($argv[1]);
+        $this->contentLength = intval($argv[2]);
+        $this->periodic = !!intval($argv[3] ?? false);
+    }
+
+    public function run()
+    {
+        $this->client = new Client($this->connectionOptions);
+        $this->client->connect();
+        $this->channel = $this->client->channel();
+        $this->channel->exchangeDeclare($this->exchange);
+        foreach ($this->queues as $routingKey => $queue) {
+            $this->channel->queueDeclare($queue);
+            $this->channel->queueBind($queue, $this->exchange, $routingKey);
+        }
+
+        if ($this->periodic) {
+            $this->addSignalHandler(SIGINT, function () {
+                $this->stopProcess();
+            });
+            $this->addTimer(1, true, function () {
+                $routingKeys = array_keys($this->queues);
+                for ($i = 0; $i < $this->numMessages; $i++) {
+                    $key = $routingKeys[array_rand($routingKeys)];
+                    $content = str_repeat('x', $this->contentLength);
+                    $this->channel->publish($content, [], $this->exchange, $key);
+                }
+            });
+            $this->process();
+        } else {
+            $routingKeys = array_keys($this->queues);
+            for ($i = 0; $i < $this->numMessages; $i++) {
+                $key = $routingKeys[array_rand($routingKeys)];
+                $content = str_repeat('x', $this->contentLength);
+                $this->channel->publish($content, [], $this->exchange, $key);
+            }
+        }
+
+        echo "done!\n";
+    }
+
+    public function onMessage(string $workerID, \Archman\Whisper\Message $msg)
+    {}
+})->run();
