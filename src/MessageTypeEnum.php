dc1f5db4960cdb612024cd2194a61b6461d27675
hsldymq <hsldymq@qq.com>
2019-07-03 15:44:08 +0800

    add message type

--- src/MessageTypeEnum.php
+++ src/MessageTypeEnum.php
@@ -9,7 +9,7 @@ class MessageTypeEnum
     // 正常队列消息
     const QUEUE = 0;
 
-    // worker已处理完消息
+    // worker已处理完队列消息
     const PROCESSED = 1;
 
     // worker告知dispatcher不要投放新消息
@@ -26,4 +26,7 @@ class MessageTypeEnum
 
     // 对于主动退出模式,dispatcher收到I_QUIT消息后,告知dispatcher准备好接受worker的退出
     const ROGER_THAT = 6;
+
+    // worker处理完自定义消息
+    const CUSTOM_MESSAGE_PROCESSED = 7;
 }
